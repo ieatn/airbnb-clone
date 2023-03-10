d29c8d0bbf783b9bca2a76c95f48d6af0a123064
ieatn <ieatnubcake@gmail.com>
2023-03-10 11:43:59 -0800

    banner done

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({exploreData}) => {
+
   return (
     <div>
       <Head>
@@ -12,9 +14,37 @@ const Home: NextPage = () => {
       </Head>
 
       <Navbar />
+      <Banner />
       
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          {/* FETCH FROM API */}
+          {exploreData.map((data) => (
+            <div key={data.id}>
+              <p>{data.name}</p>
+              <img src={data.imageUrl} alt={data.name} />
+              <p>{`${data.price} ${data.priceCurrency}`}</p>
+              <p>{`${data.lat}, ${data.lng}`}</p>
+            </div>
+          ))}
+
+        </section>
+      </main>
+
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const res = await fetch('https://cdn.rawgit.com/abbassiddiqi/airbnb-api/bbd1300a/flats.json')
+  const exploreData = await res.json()
+  return {
+    props: {
+      exploreData
+    }
+  }
+}
+
 
 export default Home
